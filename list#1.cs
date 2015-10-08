58af73a45cfe97b9e0443623e76354591f5b4a1f
Your Name <you@example.com>
2015-10-08 14:16:27 -0400

    new

---
+++ list#1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication66
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> birthYears = new List<int>() {1993,1970,1955,1967 };
+            birthYears.Add(1992);
+            
+            StreamWriter write = new StreamWriter("..\\docs.txt ");
+            using (write)
+            foreach (int year in birthYears)
+            {
+                    write.WriteLine(year);
+
+            }
+
+          
+
+
+        }
+    }
+}
